2fe60e595117b3a1ce74b41178031d66ddc98b4c
chenfengbin <1838272147@qq.com>
2022-08-01 20:56:05 +0800

    todo-test init

--- src/App.js
+++ src/App.js
@@ -1,202 +1,91 @@
-import "./index.css";
-import avatar from "./images/avatar.png";
+import { Input, Table, Space, Popconfirm } from "antd";
+import axios from "axios";
 import React from "react";
-// 使用uuid，通过它叫v4，我们将其重命名一下
-import { v4 as uuid } from "uuid";
+import "./App.css";
 
-// 时间格式化
-function formatDate(time) {
-  return `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}`;
-}
+const { Search } = Input;
 
 class App extends React.Component {
   state = {
-    // hot: 热度排序  time: 时间排序
-    tabs: [
+    list: [],
+    columns: [
       {
-        id: 1,
-        name: "热度",
-        type: "hot",
+        title: "任务编号",
+        dataIndex: "id",
+        key: "id",
       },
       {
-        id: 2,
-        name: "时间",
-        type: "time",
-      },
-    ],
-    active: "hot",
-    list: [
-      {
-        id: 1,
-        author: "刘德华",
-        comment: "给我一杯忘情水",
-        time: new Date("2021-10-10 09:09:00"),
-        // 1: 点赞 0：无态度 -1:踩
-        attitude: 1,
+        title: "任务名称",
+        dataIndex: "name",
+        key: "name",
       },
       {
-        id: 2,
-        author: "周杰伦",
-        comment: "哎哟，不错哦",
-        time: new Date("2021-10-11 09:09:00"),
-        // 1: 点赞 0：无态度 -1:踩
-        attitude: 0,
+        title: "任务描述",
+        dataIndex: "des",
+        key: "des",
       },
       {
-        id: 3,
-        author: "五月天",
-        comment: "不打扰，是我的温柔",
-        time: new Date("2021-10-11 10:09:00"),
-        // 1: 点赞 0：无态度 -1:踩
-        attitude: -1,
+        title: "操作",
+        dataIndex: "do",
+        key: "do",
+        render: (text, record) => (
+          <Space size="middle">
+            <Popconfirm
+              title="确定要删除吗?"
+              onConfirm={() => this.handleDelete(record.id)}
+            >
+              <a href="#">删除</a>
+            </Popconfirm>
+          </Space>
+        ),
       },
     ],
-    comment: "请输入内容", // 评论里面的内容
   };
 
-  // tab切换回调
-  switchTab = (type) => {
-    console.log("切换", type);
-    // 思路：点击谁就把谁的type属性交给state中的active
+  // 搜索
+  onSearch = async (value) => {
+    const res = await axios.get(`http://localhost:3001/data/?q=${value}`);
     this.setState({
-      active: type,
+      list: res.data,
     });
   };
-  // 受控组件的回调
-  textareaChange = (e) => {
+  // 删除
+  handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/data/${id}`);
+    this.loadList();
+  };
+  // 加载列表
+  loadList = async () => {
+    const res = await axios.get("http://localhost:3001/data");
+    console.log(res);
     this.setState({
-      comment: e.target.value,
+      list: res.data,
     });
   };
-  // 发表评论组件的回调
-  submitComment = () => {
-    // 提交评论 state.list 后面添加一项新的内容
-    this.setState({
-      list: [
-        ...this.state.list,
-        {
-          id: uuid(), // 独一无二的值 ？
-          author: "狠人大帝",
-          comment: this.state.comment,
-          time: new Date(),
-          // 1: 点赞 0：无态度 -1:踩
-          attitude: 0,
-        },
-      ],
-    });
-  };
-  // 删除评论的回调
-  delComment = (id) => {
-    console.log(id);
-    this.setState({
-      list: this.state.list.filter((item) => item.id !== id),
-    });
-  };
-
-  // 切换喜欢
-  toggleLike = (curItem) => {
-    // 逻辑： 如果是1，就改为 0 ，否则改为1   - attitude
-    console.log(curItem);
-    // attitude id，作为修改谁的判断条件
-    const { attitude, id } = curItem;
-    this.setState({
-      list: this.state.list.map((item) => {
-        // 判断条件 如果item.id === id , 把item的attitude属性修改一下
-        // 否则就原样
-        if (item.id === id) {
-          return {
-            ...item,
-            attitude: item.attitude === 1 ? 0 : 1,
-          };
-        } else {
-          return item;
-        }
-      }),
-    });
-  };
+  componentDidMount() {
+    // 发送接口请求
+    this.loadList();
+  }
   render() {
     return (
-      <div className="App">
-        <div className="comment-container">
-          {/* 评论数 */}
-          <div className="comment-head">
-            <span>5 评论</span>
-          </div>
-          {/* 排序 */}
-          <div className="tabs-order">
-            <ul className="sort-container">
-              {this.state.tabs.map((tab) => (
-                <li
-                  onClick={() => this.switchTab(tab.type)}
-                  key={tab.id}
-                  className={tab.type === this.state.active ? "on" : ""}
-                >
-                  按{tab.name}排序
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          {/* 添加评论 */}
-          <div className="comment-send">
-            <div className="user-face">
-              <img className="user-head" src={avatar} alt="" />
-            </div>
-            <div className="textarea-container">
-              {/* 输入框 受控组件 */}
-              <textarea
-                cols="80"
-                rows="5"
-                placeholder="发条友善的评论"
-                className="ipt-txt"
-                value={this.state.comment}
-                onChange={this.textareaChange}
-              />
-              <button className="comment-submit" onClick={this.submitComment}>
-                发表评论
-              </button>
-            </div>
-            <div className="comment-emoji">
-              <i className="face"></i>
-              <span className="text">表情</span>
-            </div>
-          </div>
-
-          {/* 评论列表 */}
-          <div className="comment-list">
-            {this.state.list.map((item) => (
-              <div className="list-item" key={item.id}>
-                <div className="user-face">
-                  <img className="user-head" src={avatar} alt="" />
-                </div>
-                <div className="comment">
-                  <div className="user">{item.author}</div>
-                  <p className="text">{item.comment}</p>
-                  <div className="info">
-                    <span className="time">{formatDate(item.time)}</span>
-                    <span
-                      onClick={() => this.toggleLike(item)}
-                      className={item.attitude === 1 ? "like liked" : "like"}
-                    >
-                      <i className="icon" />
-                    </span>
-                    <span
-                      className={item.attitude === -1 ? "hate hated" : "hate"}
-                    >
-                      <i className="icon" />
-                    </span>
-                    <span
-                      className="reply btn-hover"
-                      onClick={() => this.delComment(item.id)}
-                    >
-                      删除
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+      <div className="container">
+        <div className="search-box">
+          <Search
+            placeholder="请输入关键词"
+            allowClear
+            enterButton="搜索"
+            size="large"
+            onChange={this.inputChange}
+            value={this.state.keyword}
+            onSearch={this.onSearch}
+          />
         </div>
+        <Table
+          bordered
+          dataSource={this.state.list}
+          columns={this.state.columns}
+          pagination={false}
+        />
       </div>
     );
   }
